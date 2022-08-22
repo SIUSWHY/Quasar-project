6dd025861aabebdccaa94cc40e47cd687521787f
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-08-22 17:54:10 +0300

    add logic to messages

--- src/components/Chat/store/actions.ts
+++ src/components/Chat/store/actions.ts
@@ -1,9 +1,13 @@
 import { ActionTree } from 'vuex';
-import { SET_NEW_MESSAGE } from './mutationTypes';
+import { SET_NEW_MESSAGE, SET_NEW_MESSAGE_FROM_COMPANION } from './mutationTypes';
 import { RootState, ChatData } from './types';
 
 export const actions: ActionTree<ChatData, RootState> = {
-  pushNewMessage({ commit }, message) {
-    commit(SET_NEW_MESSAGE, message);
+  pushNewMessage({ commit, state }, message) {
+    if (message.name === state.messages[state.messages.length - 1].name) {
+      commit(SET_NEW_MESSAGE_FROM_COMPANION, message);
+    } else {
+      commit(SET_NEW_MESSAGE, message);
+    }
   },
 };
--- src/components/Chat/store/mutation.ts
+++ src/components/Chat/store/mutation.ts
@@ -1,9 +1,12 @@
 import { MutationTree } from 'vuex';
-import { SET_NEW_MESSAGE } from './mutationTypes';
+import { SET_NEW_MESSAGE, SET_NEW_MESSAGE_FROM_COMPANION } from './mutationTypes';
 import { ChatData } from './types';
 
 export const mutations: MutationTree<ChatData> = {
   [SET_NEW_MESSAGE](state, message) {
     state.messages.push(message);
+  },
+  [SET_NEW_MESSAGE_FROM_COMPANION](state, message) {
+    state.messages[state.messages.length - 1].messageText.push(message.messageText[0]);
   },
 };
