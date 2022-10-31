91a81a69b1d4928c60b99927cd872af145c80344
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-10-31 17:58:25 +0300

    add https connection

--- src/components/Tools/Settings/Pages/Gadgets/index.ts
+++ src/components/Tools/Settings/Pages/Gadgets/index.ts
@@ -1,4 +1,6 @@
+import { socket } from 'src/SocketInstance';
 import { defineComponent, ref } from 'vue';
+import { mapGetters } from 'vuex';
 const { QrcodeStream } = require('vue3-qrcode-reader');
 
 export default defineComponent({
@@ -8,20 +10,33 @@ export default defineComponent({
     return {
       isAddNewGadget: ref(false),
       err: ref(),
-      dencodeString: ref('')
+      dencodeString: ref(''),
     };
   },
   methods: {
+    ...mapGetters('appData', {
+      getCurrentUser: 'getCurrentUser',
+    }),
     goBack() {
       this.$router.push('/chat_layout/settings/main');
     },
     onDecode(decodedString: string) {
       console.log(decodedString);
-      this.dencodeString = decodedString
+      this.dencodeString = decodedString;
+      this.isAddNewGadget = false;
+      this.authUserByQR(decodedString);
     },
     async logErrors(promise: Promise<any>) {
-      promise.catch(console.error)
-      this.err = await promise.catch(console.error)
-    }
+      promise.catch(console.error);
+      this.err = await promise.catch(console.error);
+    },
+    authUserByQR(decodedString: string) {
+      const user = this.getCurrentUser();
+
+      socket.emit('send_data_for_auth_user_by_qr', {
+        socketId: decodedString,
+        userData: user,
+      });
+    },
   },
 });
